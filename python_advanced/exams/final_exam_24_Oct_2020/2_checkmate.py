8774b20208208235d9d71a8d60b6c32679f310f1
Petar Marinov <xahcvd91@gmail.com>
2020-10-24 13:34:45 +0300

    Update 2_checkmate.py

--- python_advanced/exams/final_exam_24_Oct_2020/2_checkmate.py
+++ python_advanced/exams/final_exam_24_Oct_2020/2_checkmate.py
@@ -5,8 +5,8 @@ def find_king(matrix):
                 return i, j
 
 
-def is_valid(matrix, row, col):
-    return row in range(len(matrix)) and col in range(len(matrix))
+def is_valid(row, col):
+    return row in range(8) and col in range(8)
 
 
 def output(matrix):
@@ -16,14 +16,12 @@ def output(matrix):
 
     for i in range(8):
         pos_1, pos_2 = king_pos[0] + directions[i][0], king_pos[1] + directions[i][1]
-        if matrix[pos_1][pos_2] == 'Q':
-            result.append([pos_1, pos_2])
-            continue
-        while is_valid(matrix, pos_1, pos_2):
+        while is_valid(pos_1, pos_2):
             if matrix[pos_1][pos_2] == 'Q':
                 result.append([pos_1, pos_2])
                 break
-            pos_1, pos_2 = pos_1 + directions[i][0], pos_2 + directions[i][1]
+            pos_1 += directions[i][0]
+            pos_2 += directions[i][1]
 
     if result:
         return '\n'.join([str(x) for x in result])
