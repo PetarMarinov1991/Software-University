b73f4e7cf4eb5d35b60032bf02804d8b94239c38
Petar Marinov <xahcvd91@gmail.com>
2020-11-18 03:40:39 +0200

    .

--- python_advanced/3_multidimensional_lists/2_exercise/9_radioactive_mutant_vampire_bunnies.py
+++ python_advanced/3_multidimensional_lists/2_exercise/9_radioactive_mutant_vampire_bunnies.py
@@ -18,7 +18,7 @@ def mutate_bunnies(matrix, steps):
         'D': [1, 0]
     }
     for step in range(len(steps)):
-        current_move = steps[step]
+        move = steps[step]
         new_matrix = [['.' for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
@@ -29,7 +29,7 @@ def mutate_bunnies(matrix, steps):
                         if valid_range(step[0], step[1], matrix):
                             if new_matrix[step[0]][step[1]] == '.':
                                 new_matrix[step[0]][step[1]] = 'B'
-        player_move = [player_pos[0] + directions[current_move][0], player_pos[1] + directions[current_move][1]]
+        player_move = [player_pos[0] + directions[move][0], player_pos[1] + directions[move][1]]
         matrix_output = '\n'.join([''.join([str(x) for x in new_matrix[i]]) for i in range(len(new_matrix))])
         if valid_range(player_move[0], player_move[1], matrix):
             if new_matrix[player_move[0]][player_move[1]] == 'B':
