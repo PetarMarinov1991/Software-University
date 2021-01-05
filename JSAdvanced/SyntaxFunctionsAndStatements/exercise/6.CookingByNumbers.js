2f8d205a6bc09101ed725b3b85e3726dc300610f
Petar Marinov <xahcvd91@gmail.com>
2021-01-05 10:04:37 +0200

    solved cookingByNumbers

---
+++ JSAdvanced/SyntaxFunctionsAndStatements/exercise/6.CookingByNumbers.js
@@ -0,0 +1,24 @@
+function solve([start, ...rest]) {
+    let num = parseInt(start);
+    let result = [];
+
+    for (i=0; i < rest.length; i++) {
+        let operation = rest[i];
+        if (operation == 'chop') {
+            num /= 2;
+        } else if (operation == 'dice') {
+            num = Math.sqrt(num)
+        } else if (operation == 'spice') {
+            num += 1
+        } else if (operation == 'bake') {
+            num *= 3
+        } else if (operation == 'fillet') {
+            num *= 0.8
+        }
+        result.push(num.toFixed(1));
+    }
+    return result.join('\n')
+}
+
+// console.log(solve(['32', 'chop', 'chop', 'chop', 'chop', 'chop']))
+// console.log(solve(['9', 'dice', 'spice', 'chop', 'bake', 'fillet']))
