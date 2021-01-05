37a185062e3880b3a8000ce8651c2997db128539
Petar Marinov <xahcvd91@gmail.com>
2021-01-05 08:22:11 +0200

    fixed and solved 100/100

--- JSAdvanced/SyntaxFunctionsAndStatements/exercise/4.TimeToWalk.js
+++ JSAdvanced/SyntaxFunctionsAndStatements/exercise/4.TimeToWalk.js
@@ -3,31 +3,25 @@ function calculateTimeToWalk(steps, footprint, speed) {
     let restCount = Math.floor(distance / 500);
     let ms = speed * 0.277777778;
 
-    let timeInSeconds = distance / ms;
-    let seconds = timeInSeconds % (24 * 3600)
-    let hours = Math.floor(seconds / 3600)
-    let minutes = Math.floor(seconds / 60) + restCount
-    seconds %= 60
-    seconds = Math.round(seconds)
+    let seconds = (distance / ms) + restCount * 60;
+    let h = Math.floor(seconds % (3600 * 24) / 3600);
+    let m = Math.floor(seconds % 3600 / 60);
+    let s = Math.round(seconds % 60);
 
-    if (seconds >= 60) {
-        seconds -= 60
-        minutes += 1
-    }
-    if (minutes >= 60) {
-        minutes -= 60
-        hours += 1
+    if (s == 60) {
+        s -= 60
+        m += 1
     }
 
-    let result = `${hours}:${minutes}:${seconds}`;
-    if (hours < 10) {
-        result = '0' + result
+    function pad(num, size=2) {
+        num = num.toString();
+        while (num.length < size) num = "0" + num;
+        return num
     }
-    if (seconds == 0) {
-        result = result + '0'
-    }
+
+    let result = `${pad(h)}:${pad(m)}:${pad(s)}`
     return result
 }
 
-// console.log(calculateTimeToWalk(4000, 2, 5));
+// console.log(calculateTimeToWalk(4000, 0.6, 5));
 // console.log(calculateTimeToWalk(2564, 0.70, 5.5));
