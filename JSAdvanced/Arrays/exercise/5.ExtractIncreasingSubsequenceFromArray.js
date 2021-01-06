3ab8f4ee365eaf15006fc6d9631a75f743480f07
Petar Marinov <xahcvd91@gmail.com>
2021-01-06 15:12:31 +0200

    solved extractEncreasingSubsequenceFromArray

---
+++ JSAdvanced/Arrays/exercise/5.ExtractIncreasingSubsequenceFromArray.js
@@ -0,0 +1,13 @@
+function solve(arr) {
+    let currentBiggest = Number.MIN_SAFE_INTEGER
+    for (const i in arr) {
+        if (arr[i] > currentBiggest) {
+            console.log(arr[i])
+            currentBiggest = arr[i]
+        }
+    }
+}
+
+// solve([1, 3, 8, 4, 10, 12, 3, 2, 24])
+// solve([1, 2, 3, 4])
+// solve([20, 3, 2, 15, 6, 1])
