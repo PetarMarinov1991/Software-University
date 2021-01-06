bcd9a14bfa09d6568462454c46a1757bfc14ca49
Petar Marinov <xahcvd91@gmail.com>
2021-01-06 11:45:22 +0200

    solved lastKNumbersSequence

---
+++ JSAdvanced/Arrays/lab/4.LastKNumbers.js
@@ -0,0 +1,16 @@
+function numberSequence(len, k) {
+    let seq = [1]
+
+    for (i = 1; i < len; i++) {
+        if (seq.length < k) {
+            seq.push(seq.reduce((a, b) => a + b, 0))
+        } else {
+           seq.push(seq.slice(i - k, len).reduce((a, b) => a + b, 0))
+        }
+    }
+
+    return seq.join(' ')
+}
+
+// console.log(numberSequence(6 , 3))
+// console.log(numberSequence(8 , 2))
