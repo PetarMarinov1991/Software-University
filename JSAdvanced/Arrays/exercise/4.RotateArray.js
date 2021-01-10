4b6d4c689d593141ceea7b168081e38ca24f0f58
Petar Marinov <xahcvd91@gmail.com>
2021-01-10 13:34:46 +0200

    some changes in rotateArray

--- JSAdvanced/Arrays/exercise/4.RotateArray.js
+++ JSAdvanced/Arrays/exercise/4.RotateArray.js
@@ -1,13 +1,12 @@
-function solve(arr) {
+function rotate(arr) {
     let rotations = Number(arr[arr.length - 1])
     let rest = arr.splice(0, arr.length - 1)
 
     for (i=0; i < rotations; i++) {
-        rest.unshift(rest[rest.length - 1])
-        rest.pop()
+        rest.unshift(rest.pop())
     }
     return rest.join(' ')
 }
 
-// console.log(solve(['1', '2', '3', '4', '2']))
-// console.log(solve(['Banana', 'Orange', 'Coconut', 'Apple', '15']))
+// console.log(rotate(['1', '2', '3', '4', '2']))
+// console.log(rotate(['Banana', 'Orange', 'Coconut', 'Apple', '15']))
