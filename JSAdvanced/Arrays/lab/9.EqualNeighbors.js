bf5682857e73457ebc977d9af0a92e45596de7be
Petar Marinov <xahcvd91@gmail.com>
2021-01-10 12:40:23 +0200

    solved equalNeighbors

--- JSAdvanced/Arrays/lab/9.EqualNeighbors.js
+++ JSAdvanced/Arrays/lab/9.EqualNeighbors.js
@@ -1,8 +1,14 @@
 function equalNeighbors(matrix) {
     let pairs = 0
     for (i=0; i < matrix.length - 1; i++) {
-       for (j=0; j < matrix[i].length - 1; j++) {
-            if (matrix[i][j] == matrix[i + 1][j] || matrix[i][j] == matrix[i][j + 1]) {
+       for (j=0; j < matrix[i].length; j++) {
+            if (matrix[i][j] == matrix[i + 1][j]) {
+                pairs += 1
+            }
+            if (matrix[i][j] == matrix[i][j + 1]) {
+                pairs += 1
+            }
+            if (i == matrix.length - 2 && matrix[i + 1][j] == matrix[i + 1][j + 1]) {
                 pairs += 1
             }
         }
@@ -23,5 +29,12 @@ function equalNeighbors(matrix) {
 //         ['test', 'yes', 'yo', 'ho'],
 //         ['well', 'done', 'yo', '6'],
 //         ['not', 'done', 'yet', '5']
+//     ]
+// ))
+// console.log(equalNeighbors(
+//     [
+//         [2, 2, 5, 7, 4],
+//         [4, 0, 5, 3, 4],
+//         [2, 5, 5, 4, 2]
 //     ]
 // ))
