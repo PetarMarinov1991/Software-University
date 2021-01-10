9fe0ea7d2964389288c2dc9f996370f1e9ec6266
Petar Marinov <xahcvd91@gmail.com>
2021-01-10 12:12:52 +0200

    solved smallestTwoNumbers

--- JSAdvanced/Arrays/lab/6.SmallestTwoNumbers.js
+++ JSAdvanced/Arrays/lab/6.SmallestTwoNumbers.js
@@ -2,8 +2,16 @@ function smallestTwoNumbers(args) {
     let min = Math.min.apply(null, args)
     let secondMin = Math.min.apply(null, args.filter(n => n != min))
 
+    if (secondMin === Infinity) {
+        secondMin = min
+    }
+    if (args.length == 1) {
+        return `${min}`
+    }
+    
     return `${min} ${secondMin}`
 }
 
 // console.log(smallestTwoNumbers([30, 15, 50, 5]))
 // console.log(smallestTwoNumbers([3, 0, 10, 4, 7, 3]))
+// console.log(smallestTwoNumbers([3]))
