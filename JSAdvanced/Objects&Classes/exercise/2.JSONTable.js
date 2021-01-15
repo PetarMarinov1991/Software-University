2ec26ae522ce3aaee2edccccb0d96218d514c323
Petar Marinov <xahcvd91@gmail.com>
2021-01-15 15:39:51 +0200

    solved JSONTable

---
+++ JSAdvanced/Objects&Classes/exercise/2.JSONTable.js
@@ -0,0 +1,25 @@
+function JSONTable(input) {
+    let result = '<table>\n'
+    let employees = []
+
+    input.forEach(element => {
+        employees.push(JSON.parse(element))
+    })
+
+    employees.forEach(employee => {
+        result += '\t<tr>\n'
+
+        Object.values(employee).forEach(value => {
+            result += `\t\t<td>${value}</td>\n`
+        })
+    })
+    return result + '</table>'
+}
+
+// console.log(JSONTable(
+//     [
+//         '{"name":"Pesho","position":"Promenliva","salary":100000}',
+//         '{"name":"Teo","position":"Lecturer","salary":1000}',
+//         '{"name":"Georgi","position":"Lecturer","salary":1000}'
+//     ]
+// ))
