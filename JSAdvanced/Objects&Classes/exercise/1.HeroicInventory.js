e08590f8c080367de883f2611789ac2c3c72c6a4
Petar Marinov <xahcvd91@gmail.com>
2021-01-15 12:55:14 +0200

    solved heroicInventory

---
+++ JSAdvanced/Objects&Classes/exercise/1.HeroicInventory.js
@@ -0,0 +1,21 @@
+function heroicInventory(input) {
+    let result = []
+
+    for (data of input) {
+        let [name, level, items] = data.split(' / ')
+        level = Number(level)
+        items = items ? items.split(', ') : []
+
+        result.push({name, level, items})
+    }
+
+    return JSON.stringify(result)
+}
+
+// console.log(heroicInventory(
+//     [
+//         'Isacc / 25 / Apple, GravityGun',
+//         'Derek / 12 / BarrelVest, DestructionSword',
+//         'Hes / 1 / Desolator, Sentinel, Antara'
+//     ]
+// ))
