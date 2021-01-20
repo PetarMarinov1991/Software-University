af9c802c0a2f242a52e1d58aa1b8282a00bbe0ae
Petar Marinov <xahcvd91@gmail.com>
2021-01-20 17:51:08 +0200

    changed and solved lowestPricesInCities

--- JSAdvanced/Objects&Classes/lab/5.LowestPricesInCities.js
+++ JSAdvanced/Objects&Classes/lab/5.LowestPricesInCities.js
@@ -1,38 +1,22 @@
 function lowestPrices(arr) {
-    let data = arr
-        .map(row => row.split('|')
-        .map(x => x.trim()))
-
     let result = {}
 
-    data.forEach(row => {
-        let town = row[0]
-        let product = row[1]
-        let price = Number(row[2])
+    arr.forEach(row => {
+        let data = row.split(' | ')
+        let town = data[0]
+        let product = data[1]
+        let price = Number(data[2])
 
-        if (result.hasOwnProperty(product)){
-            if (result[product][0] < price) {
+        if (result[product]){
+            if (result[product][0] <= price) {
                 return
             }
         }   
 
-        result[product] = [price, town]
-        
-    });
+        result[product] = [price, town]      
+    })
 
-    for (key in result) {
-        console.log(`${key} -> ${result[key][0]} (${result[key][1]})`)
+    for (const [product, value] of Object.entries(result)) {
+        console.log(`${product} -> ${value[0]} (${value[1]})`)
     }
 }
-
-// lowestPrices(
-//     [
-//         'Sample Town | Sample Product | 1000',
-//         'Sample Town | Orange | 2',
-//         'Sample Town | Peach | 1',
-//         'Sofia | Orange | 3',
-//         'Sofia | Peach | 2',
-//         'New York | Sample Product | 1000.1',
-//         'New York | Burger | 10'
-//     ]
-// )
