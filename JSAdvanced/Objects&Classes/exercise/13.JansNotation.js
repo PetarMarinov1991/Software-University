aa8bd5cb9de13cdd7b45e080e18d4766943c7c04
Petar Marinov <xahcvd91@gmail.com>
2021-01-21 11:33:26 +0200

    changed in python style :D

--- JSAdvanced/Objects&Classes/exercise/13.JansNotation.js
+++ JSAdvanced/Objects&Classes/exercise/13.JansNotation.js
@@ -2,11 +2,8 @@ function solve(input) {
     let arr = []
     
     input.forEach(element => {
-        if (typeof element === 'number') {
-            arr.push(Number(element))
-        } else {
-            doMath(arr, element)
-        }
+        if (typeof element === 'number') arr.push(Number(element)) 
+        else doMath(arr, element)
     })
 
     let output = arr.length >= 2 ? 'Error: too many operands!' : arr.toString()
@@ -17,19 +14,10 @@ function solve(input) {
         let idx2 = numbers.length - 1
 
         if (numbers.length < 2) console.log('Error: not enough operands!')
-
-        if (operator === '+') {
-            numbers[idx1] += numbers[idx2]
-        }
-        else if (operator === '-') {
-            numbers[idx1] -= numbers[idx2]
-        }
-        else if (operator === '*') {
-            numbers[idx1] *= numbers[idx2]
-        }
-        else if (operator === '/') {
-            numbers[idx1] /= numbers[idx2]
-        }
+        else if (operator === '+') numbers[idx1] += numbers[idx2]
+        else if (operator === '-') numbers[idx1] -= numbers[idx2]
+        else if (operator === '*') numbers[idx1] *= numbers[idx2]
+        else if (operator === '/') numbers[idx1] /= numbers[idx2]
         numbers.splice(-1, 1)
     }
 }
