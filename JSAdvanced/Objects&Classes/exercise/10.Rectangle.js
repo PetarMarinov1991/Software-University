92523b741ad75add0bf1eb11ec9e68af8a7c999b
Petar Marinov <xahcvd91@gmail.com>
2021-01-21 15:48:04 +0200

    changed and solved rectangle for 100/100

--- JSAdvanced/Objects&Classes/exercise/10.Rectangle.js
+++ JSAdvanced/Objects&Classes/exercise/10.Rectangle.js
@@ -1,11 +1,12 @@
 function rectangle(width, height, color) {
+    let capitalize = color => color[0].toUpperCase() + color.slice(1)
+    let calcArea = () => width * height
+
     let rect = {
-        width: width,
-        height: height,
-        color: color,
-        calcArea: function() {
-           return width * height
-        }
+        width,
+        height,
+        color: capitalize(color),
+        calcArea
     }
 
     return rect
