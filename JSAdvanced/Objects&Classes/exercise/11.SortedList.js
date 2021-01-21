c3a7e6ee0841058db435c424d2d0568ea30ddca8
Petar Marinov <xahcvd91@gmail.com>
2021-01-21 16:34:01 +0200

    solved sortedList

--- JSAdvanced/Objects&Classes/exercise/11.SortedList.js
+++ JSAdvanced/Objects&Classes/exercise/11.SortedList.js
@@ -1,30 +1,21 @@
 function createSortedList() {
-    let sortedList = {
-        list: [],
-        add: function(num) {
-            this.list.push(num)
-            this.list.sort((a, b) => a - b)
-        },
-        push: function(num) {
-            return add(num) // needed?
-        },
-        remove: function(index) {
-            if (validIndex(index)) {
-                this.list.splice(index, 1)
-            }
-            return 'IndexError'
-        },
-        get: function(index) {
-            if (validIndex(index)) {
-                return this.list[index]
-            }
-            return 'IndexError'
-        },
-        size: 0,
-        // size: this.list.length ...error?
-        // size: sortedList['list'].length ...error?
+    let list = []
+
+    function add(num) {
+        list.push(num), list.sort((a, b) => a - b), this.size++
     }
-    let validIndex = index => index >= 0 && index < sortedList['list'].length
-    sortedList['size'] = sortedList['list'].length // still 0 ? the list is not saved?
-    return sortedList
+    function remove(index) {
+        if (index >= 0 && index < list.length) list.splice(index, 1), this.size--
+    }
+    function get(index) {
+        if (index >= 0 && index < list.length) return list[index]
+    }
+    
+    let listAsObject = {
+        add, 
+        remove, 
+        get, 
+        size: 0
+    }
+    return listAsObject
 }
