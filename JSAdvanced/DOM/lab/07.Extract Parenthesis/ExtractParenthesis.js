ca21b1e435ebcdf51e29869c64d7f3b35fb685ff
Petar Marinov <xahcvd91@gmail.com>
2021-01-22 20:33:51 +0200

    solved exctractParenthesis

--- JSAdvanced/DOM/lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ JSAdvanced/DOM/lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,14 @@
 function extract(content) {
+    const text = document.getElementById(content).textContent
+    const regex = /\((.*?)\)/gm
+    
+    let result = []
 
-}
+    let match = regex.exec(text)
+
+    while (match) {
+        result.push(match[1])
+        match = regex.exec(text)
+    }
+    return result.join('; ')
+} 
