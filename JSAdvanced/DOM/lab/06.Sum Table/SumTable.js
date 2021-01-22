6d1beaabe7e839f72e1fdb031e168d268d46a73d
Petar Marinov <xahcvd91@gmail.com>
2021-01-22 19:58:31 +0200

    solved sumTable

--- JSAdvanced/DOM/lab/06.Sum Table/SumTable.js
+++ JSAdvanced/DOM/lab/06.Sum Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
+    const rows = document.getElementsByTagName('tr')
+    let sum = 0
 
+    for (i=1; i < rows.length - 1; i++) {
+        sum += Number(rows[i].lastElementChild.textContent)
+    }
+    
+    document.getElementById('sum').textContent = sum
 }
