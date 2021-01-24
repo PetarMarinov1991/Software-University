da4803af8938323d88cc816e48d719e3c23517f7
Petar Marinov <xahcvd91@gmail.com>
2021-01-24 15:36:40 +0200

    solved accordion

--- JSAdvanced/DOM/exercise/03. Accordion/accordion.js
+++ JSAdvanced/DOM/exercise/03. Accordion/accordion.js
@@ -1,3 +1,11 @@
 function toggle() {
-    console.log('TODO:...');
+    const button = document.getElementsByClassName("button")[0]
+    const text = document.getElementById('extra')
+
+    if (text.style.display === 'none' || text.style.display === '') 
+        text.style.display = 'block',
+        button.textContent = 'Less'
+    else 
+        text.style.display = 'none',
+        button.textContent = 'More'    
 }
