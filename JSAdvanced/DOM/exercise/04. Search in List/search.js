05b8d3db4c3b9dd2e5c82336d62ea049aa803cc1
Petar Marinov <xahcvd91@gmail.com>
2021-01-24 16:39:58 +0200

    solved searchInList

--- JSAdvanced/DOM/exercise/04. Search in List/search.js
+++ JSAdvanced/DOM/exercise/04. Search in List/search.js
@@ -1,3 +1,16 @@
 function search() {
-   // TODO
+   const towns = [...document.getElementsByTagName('li')]
+   const searchedWord = document.getElementById('searchText').value
+
+   let matchesCount = 0
+
+   towns.forEach(element => {
+      if (element.textContent.includes(searchedWord)) {
+         element.style.textDecoration = 'underline'
+         element.style.fontWeight = 'bold'
+         matchesCount++
+      }
+   })
+   
+   document.getElementById('result').textContent = `${matchesCount} matches found`
 }
