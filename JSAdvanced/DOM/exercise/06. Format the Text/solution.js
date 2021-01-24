5b8cc479aea6005ba281cb89fb53dbdfcff44748
Petar Marinov <xahcvd91@gmail.com>
2021-01-24 20:22:03 +0200

    solved formatText

--- JSAdvanced/DOM/exercise/06. Format the Text/solution.js
+++ JSAdvanced/DOM/exercise/06. Format the Text/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-  //TODO
+  const input = document.getElementById('input').value
+  const output = document.getElementById('output')
+
+  input
+    .match(/[^\.!\?]+[\.!\?]+/g)
+    .reduce((acc, sentence, index) => {
+      if (index % 3 === 0) acc.push([sentence]) 
+      else acc[acc.length - 1].push(sentence) 
+      return acc
+    }, [])
+    .forEach(p => {
+      let paragraph = document.createElement('p')
+      paragraph.textContent = p
+      output.appendChild(paragraph)
+    })
 }
