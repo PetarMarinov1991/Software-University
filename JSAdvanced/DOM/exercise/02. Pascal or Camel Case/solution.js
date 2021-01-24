5fe818030c1b032c0c323883a754c84b4e9c91f9
Petar Marinov <xahcvd91@gmail.com>
2021-01-24 14:44:55 +0200

    solved pascalOrCamelCase

--- JSAdvanced/DOM/exercise/02. Pascal or Camel Case/solution.js
+++ JSAdvanced/DOM/exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-  //TODO...
+  const input = (document.getElementById('text').value).split(' ')
+  const convertCase = document.getElementById('naming-convention').value
+
+  let wordsToLowerCase = []
+
+  input.forEach(word => wordsToLowerCase.push(word.toLowerCase()))
+  
+  let result = ''
+
+  wordsToLowerCase.forEach(word => {
+    let firstLetter = word[0].toUpperCase()
+    let rest = word.slice(1, word.length)
+    result += firstLetter + rest
+
+    if (convertCase == 'Pascal Case') result 
+    else if (convertCase == 'Camel Case') result = result[0].toLowerCase() + result.slice(1, result.length) 
+    else result = 'Error!'
+  })
+  document.getElementById('result').textContent = result
 }
