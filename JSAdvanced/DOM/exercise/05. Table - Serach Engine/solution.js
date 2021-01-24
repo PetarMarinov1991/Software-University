f0b86bbbd6a23c9befac95f18259d87df2437776
Petar Marinov <xahcvd91@gmail.com>
2021-01-24 18:49:36 +0200

    solved tableSearchEngine

--- JSAdvanced/DOM/exercise/05. Table - Serach Engine/solution.js
+++ JSAdvanced/DOM/exercise/05. Table - Serach Engine/solution.js
@@ -2,7 +2,15 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
-
+      const searchedWord = document.getElementById('searchField').value
+      const table = [...document.querySelectorAll('tbody tr')]
+      
+      table.forEach(row => {
+         if (row.textContent.includes(searchedWord)) {
+            row.classList.add('select')
+         } else {
+            row.classList.remove('select')
+         }
+      })
    }
 }
