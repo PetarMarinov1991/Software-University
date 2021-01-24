0ad5c59151edfb42199d73e94955e02b8f04b965
Petar Marinov <xahcvd91@gmail.com>
2021-01-24 13:39:51 +0200

    solved subtract

--- JSAdvanced/DOM/exercise/01. Subtraction/subtract.js
+++ JSAdvanced/DOM/exercise/01. Subtraction/subtract.js
@@ -1,7 +1,6 @@
 function subtract() {
-    const num1 = document.getElementById('firstNumber').value
-    const num2 = document.getElementById('secondNumber').value
-    const result = document.getElementById('result') = Number(num1) - Number(num2)
-    const newDiv = document.getElementById('wrapper').createElement('div')
-    newDiv.appendChild(result)
+    const num1 = Number(document.getElementById('firstNumber').value)
+    const num2 = Number(document.getElementById('secondNumber').value)
+    const diff = num1 - num2
+    document.getElementById('result').textContent = diff
 }
