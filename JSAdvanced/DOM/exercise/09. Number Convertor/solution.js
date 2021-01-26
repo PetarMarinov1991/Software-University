37e6fd8b941687c8101b277c5fa601499477a924
Petar Marinov <xahcvd91@gmail.com>
2021-01-26 05:07:58 +0200

    solved numberConvertor

--- JSAdvanced/DOM/exercise/09. Number Convertor/solution.js
+++ JSAdvanced/DOM/exercise/09. Number Convertor/solution.js
@@ -1,4 +1,33 @@
 function solve() {
+    let selectElement = document.querySelector('#selectMenuTo')
+    let binary = document.createElement('option')
+    let hexadecimal = document.createElement('option')
 
-    //TODO...
+    binary.value = 'binary'
+    binary.text = 'Binary'
+    hexadecimal.value = 'hexadecimal'
+    hexadecimal.text = 'Hexadecimal'
+
+    selectElement.add(binary)
+    selectElement.add(hexadecimal)
+
+    document.querySelector('button').addEventListener('click', clickEvent)
+
+    function clickEvent() {
+        let decimalNumber = +document.querySelector('#input').value
+        let convertTo = selectElement.options[selectElement.selectedIndex].text
+
+        let result = undefined
+
+        switch (convertTo) {
+            case 'Binary':
+                result = decimalNumber.toString(2)
+                break
+            case 'Hexadecimal':
+                result = decimalNumber.toString(16).toUpperCase()
+                break
+        }
+
+        document.querySelector('#result').value = result
+    }
 }
