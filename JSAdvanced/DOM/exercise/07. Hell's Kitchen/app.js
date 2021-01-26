0248a3c3b94440b21ac8d139da35479597d56f1e
Petar Marinov <xahcvd91@gmail.com>
2021-01-26 04:44:01 +0200

    solved hells kitchen

--- JSAdvanced/DOM/exercise/07. Hell's Kitchen/app.js
+++ JSAdvanced/DOM/exercise/07. Hell's Kitchen/app.js
@@ -2,61 +2,59 @@ function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick () {
-      const input = document.querySelector('#inputs>textarea').value
-         .replace(new RegExp(/[\[\]']/g), null)
-         .split('"')
+      const input = JSON.parse(document.querySelector('#inputs>textarea').value)
 
-      let restaurantsData = {}
+      let restaurants = {}
       
       input.forEach(row => {
-         let [restourantName, workers] = row.split('-')
-         restourantName = restourantName.trim()
-         
-         if (workers !== undefined) {
-            let workersInRestaurant = workers.split(',')
-            workersInRestaurant.forEach(worker => {
-               let [_, workerName, workerSalarvalue] = worker.split(' ')
-               if (!restaurantsData[restourantName]) {
-                  restaurantsData[restourantName] = {}
-               }
-               if (!restaurantsData[restourantName][workerName]) {
-                  restaurantsData[restourantName][workerName] = Number(workerSalarvalue)
-               }
-            })
+         let args = row.split(' - ')
+         let name = args[0]
+         let workersArr = args[1].split(', ')
+         let workers = []
+
+         workersArr.forEach(worker => {
+            let args = worker.split(' ')
+            let workerName = args[0]
+            let salary = Number(args[1])
+            
+            workers.push({ name: workerName, salary })
+         })
+
+         if (restaurants[name]) {
+            workers = workers.concat(restaurants[name].workers)
+         }
+
+         workers.sort((worker1, worker2) => worker2.salary - worker1.salary)
+         const bestSalary = workers[0].salary
+         const averageSalary = workers.reduce((sum, worker) => sum + worker.salary, 0) / workers.length
+
+         restaurants[name] = {
+            workers,
+            averageSalary,
+            bestSalary
          }
       })
-      let sorted = {}
-      Object.entries(restaurantsData).forEach(element => {
-         let restourantName = element[0]
-         sorted[restourantName] = {}
-         sorted[restourantName]['averageSalary'] = 0
-         Object.entries(element[1])
-            .sort((x, value) => value[1] - x[1])
-            .forEach(element => { 
-               sorted[restourantName][element[0]] = element[1]
-               sorted[restourantName]['averageSalary'] += element[1]
-            })
-            sorted[restourantName]['averageSalary'] /= Object.values(sorted[restourantName]).length - 1
-      })
       
-      let bestAverage = 0
-      let bestPlace = []
+      let bestRestaurantSalary = 0
+      let best = {}
+
+      for (const name in restaurants) {
+         if (restaurants[name].averageSalary > bestRestaurantSalary)
+            best = {
+               name, 
+               workers: restaurants[name].workers,
+               bestSalary: restaurants[name].bestSalary,
+               averageSalary: restaurants[name].averageSalary
+            }
+
+            bestRestaurantSalary = restaurants[name].averageSalary
+      }
+
       let result = ''
 
-      for (const [placeName, info] of Object.entries(sorted)) {
-         for (const [key, value] of Object.entries(info)) {
-            if (key === 'averageSalary' && value > bestAverage) {
-               bestPlace = [placeName, Object.entries(info)]
-            }
-         }
-      }
-      
-      for (let i=1; i < bestPlace[1].length; i++) {
-         result += `Name: ${bestPlace[1][i][0]} With Salary: ${bestPlace[1][i][1]} `
-      }
+      best.workers.forEach(worker => result += `Name: ${worker.name} With Salary: ${worker.salary} `)
 
-      document.querySelector('#bestRestaurant>p').textContent =
-         `Name: ${bestPlace[0]} Average Salary: ${bestPlace[1][0][1].toFixed(2)} Best Salary: ${bestPlace[1][1][1].toFixed(2)}`
+      document.querySelector('#bestRestaurant>p').textContent = `Name: ${best.name} Average Salary: ${best.averageSalary.toFixed(2)} Best Salary: ${best.bestSalary.toFixed(2)}`
       document.querySelector('#workers>p').textContent = result
    }
 }
