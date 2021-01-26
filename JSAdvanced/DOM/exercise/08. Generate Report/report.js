5608ccb7d2655ae1e5653793830de3dde85d7382
Petar Marinov <xahcvd91@gmail.com>
2021-01-26 15:27:35 +0200

    solved generateReport

--- JSAdvanced/DOM/exercise/08. Generate Report/report.js
+++ JSAdvanced/DOM/exercise/08. Generate Report/report.js
@@ -1,3 +1,32 @@
-function generateReport(colNames) {
-    //TODO
+function generateReport() {
+    const output = document.getElementById('output')
+    const checkboxes = [...document.querySelectorAll('thead th input')]
+
+    let checkedBoxesIndexes = []
+
+    checkboxes.forEach((box, idx) => {
+        if (box.checked === true) {
+            checkedBoxesIndexes.push(idx)
+        }
+    })
+
+    let result = []
+
+    const th = document.querySelectorAll('thead th')
+    const tr = [...document.querySelectorAll('tbody tr')]
+
+    for (const row of tr) {
+
+        let obj = {}
+    
+        for (const idx of checkedBoxesIndexes) {
+            let currRowArr = row.children[idx].textContent
+            let currThName = th[idx].textContent.toLowerCase().trim()
+            obj[currThName] = currRowArr
+        }
+
+        result.push(obj)
+    }
+
+    output.value = JSON.stringify(result, null, 2)
 }
