30cc1865d7c29f74bbc040b69b6cb0a40a5f4298
Petar Marinov <xahcvd91@gmail.com>
2021-01-27 20:51:04 +0200

    solved listOfItems

--- JSAdvanced/DOMManipulationsAndEvents/lab/01. List-Of-Items/app.js
+++ JSAdvanced/DOMManipulationsAndEvents/lab/01. List-Of-Items/app.js
@@ -1,3 +1,7 @@
 function addItem() {
-    console.log('TODO:...');
+    const items = document.getElementById('items')  
+    const newItem = document.getElementById('newItemText').value
+
+    items.appendChild(document.createElement("li")).textContent = newItem
+    document.getElementById('newItemText').value = null
 }
