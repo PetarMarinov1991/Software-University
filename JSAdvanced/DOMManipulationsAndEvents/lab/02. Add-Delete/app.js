8f06ba42989e499691483de7e8355131e8d6f3cb
Petar Marinov <xahcvd91@gmail.com>
2021-01-27 21:53:37 +0200

    solved add/delete

--- JSAdvanced/DOMManipulationsAndEvents/lab/02. Add-Delete/app.js
+++ JSAdvanced/DOMManipulationsAndEvents/lab/02. Add-Delete/app.js
@@ -1,3 +1,21 @@
 function addItem() {
-    //TODO...
+    const input = document.getElementById('newText').value
+    const newItem = createElement('li', input)
+
+    const deleteBtn = createElement('a', '[Delete]')
+    deleteBtn.href = '#'
+    newItem.appendChild(deleteBtn)
+
+    deleteBtn.addEventListener('click', ev => {
+        ev.target.parentNode.remove()
+    })
+
+    document.getElementById('items').appendChild(newItem)
+    document.getElementById('newText').value = null
+
+    function createElement(type, content) {
+        const element = document.createElement(type)
+        element.textContent = content
+        return element
+    }
 }
