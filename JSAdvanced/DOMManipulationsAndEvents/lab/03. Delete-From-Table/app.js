c5323d7856e10993214527fccd2ef73ff962d971
Petar Marinov <xahcvd91@gmail.com>
2021-01-31 10:52:33 +0200

    solved deleteFromTable

--- JSAdvanced/DOMManipulationsAndEvents/lab/03. Delete-From-Table/app.js
+++ JSAdvanced/DOMManipulationsAndEvents/lab/03. Delete-From-Table/app.js
@@ -1,3 +1,14 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let table = [...document.querySelectorAll('tbody tr')]
+    const input = document.querySelector('input[name="email"]')
+    let result = document.getElementById('result')
+
+    table.forEach(row => {
+        if (row.children[1].textContent == input.value) {
+            row.remove()
+            result.textContent = 'Deleted.'
+        } else {
+            result.textContent = 'Not found.'
+        }
+    })
 }
