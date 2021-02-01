0f8dc3e01dfac6270d5783c0085f242d40895096
Petar Marinov <xahcvd91@gmail.com>
2021-02-01 16:17:02 +0200

    solved highlightActive

--- JSAdvanced/DOMManipulationsAndEvents/lab/05. Highlight-Active/app.js
+++ JSAdvanced/DOMManipulationsAndEvents/lab/05. Highlight-Active/app.js
@@ -1,3 +1,14 @@
 function focus() {
-    console.log('TODO:...');
+    [...document.querySelectorAll('input')].forEach(i => {
+      i.addEventListener('focus', onFocus)
+      i.addEventListener('blur', onBlur)
+    })
+
+    function onFocus(ev) {
+        ev.target.parentNode.className = 'focused'
+    }
+
+    function onBlur(ev) {
+        ev.target.parentNode.className = ''
+    }
 }
