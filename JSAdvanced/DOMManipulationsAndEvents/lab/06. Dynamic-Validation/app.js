2aa166f8e65c9b7e2006e9f5b4d6f354ca5f53de
Petar Marinov <xahcvd91@gmail.com>
2021-02-01 16:41:25 +0200

    solved dynamicValidation

--- JSAdvanced/DOMManipulationsAndEvents/lab/06. Dynamic-Validation/app.js
+++ JSAdvanced/DOMManipulationsAndEvents/lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    console.log('TODO:...');
+    document.getElementById('email').addEventListener('change', onChange)
+
+    function onChange(ev) {
+        const email = ev.target.value
+        const regex = /^[a-z]+@[a-z]+\.[a-z]+$/
+
+        if (regex.test(email)) {
+            ev.target.className = ''
+        } else {
+            ev.target.className = 'error'
+        }
+    }
 }
