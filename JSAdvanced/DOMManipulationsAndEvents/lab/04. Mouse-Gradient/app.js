c9553a638e2091cbe6da3dded9a35bb18e005dcf
Petar Marinov <xahcvd91@gmail.com>
2021-02-01 15:26:17 +0200

    solved attachGradient

--- JSAdvanced/DOMManipulationsAndEvents/lab/04. Mouse-Gradient/app.js
+++ JSAdvanced/DOMManipulationsAndEvents/lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,11 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    document.getElementById('gradient').addEventListener('mousemove', onMove)
+    let output = document.getElementById('result')
+
+    function onMove(ev) {
+        const offsetX = ev.offsetX
+        const percent = Math.floor(offsetX / ev.target.clientWidth * 100)
+
+        output.textContent = `${percent}%`
+    }
 }
