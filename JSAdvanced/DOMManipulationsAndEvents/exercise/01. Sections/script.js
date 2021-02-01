801e1fa951d526fb93079d50681f3c807e409cbe
Petar Marinov <xahcvd91@gmail.com>
2021-02-01 22:03:12 +0200

    solved sections

--- JSAdvanced/DOMManipulationsAndEvents/exercise/01. Sections/script.js
+++ JSAdvanced/DOMManipulationsAndEvents/exercise/01. Sections/script.js
@@ -1,3 +1,22 @@
 function create(words) {
-   console.log('TODO:...');
+   const content = document.getElementById('content')
+
+   Array.from(words).forEach(word => {
+      const div = document.createElement('div')
+      const paragraph = document.createElement('p')
+      paragraph.textContent = word
+      paragraph.style.display = 'none'
+      div.appendChild(paragraph)
+      content.appendChild(div)
+   })
+
+   content.addEventListener('click', onClick)
+
+   function onClick(ev) {
+      if (ev.target.tagName === 'DIV' || ev.target.tagName === 'P') {
+         const p = ev.target.children[0] || e.target
+         const isVisible = p.style.display === 'block'
+         p.style.display = isVisible ? 'none' : 'block'
+      }
+   }
 }
