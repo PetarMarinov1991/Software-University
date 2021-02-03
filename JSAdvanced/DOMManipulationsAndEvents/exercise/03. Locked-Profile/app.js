138d32873d85837bd03a9100de08ce22ae9a155e
Petar Marinov <xahcvd91@gmail.com>
2021-02-03 17:23:53 +0200

    solved lockedProfile

--- JSAdvanced/DOMManipulationsAndEvents/exercise/03. Locked-Profile/app.js
+++ JSAdvanced/DOMManipulationsAndEvents/exercise/03. Locked-Profile/app.js
@@ -1,3 +1,15 @@
 function lockedProfile() {
-    console.log('TODO...')
+    document.getElementById('main').addEventListener('click', ev => {
+        if (ev.target.tagName === 'BUTTON') {
+            const profile = ev.target.parentNode
+            let locked = profile.querySelector('input[type=radio]:checked').value
+
+            if (locked === 'unlock') {
+                let div = profile.querySelector('div')
+                let visible = div.style.display === 'block'
+                div.style.display = visible ? 'none' : 'block'
+                ev.target.textContent = visible ? 'Show more' : 'Hide it'
+            }
+        }
+    })
 }
