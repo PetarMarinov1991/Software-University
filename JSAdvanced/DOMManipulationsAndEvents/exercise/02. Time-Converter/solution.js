6316a7608fd3af87a879a863463de636fa8a410a
Petar Marinov <xahcvd91@gmail.com>
2021-02-03 14:29:40 +0200

    solved timeConverter

--- JSAdvanced/DOMManipulationsAndEvents/exercise/02. Time-Converter/solution.js
+++ JSAdvanced/DOMManipulationsAndEvents/exercise/02. Time-Converter/solution.js
@@ -1,4 +1,39 @@
 function attachEventsListeners() {
+    const daysBtn = document.getElementById('daysBtn')
+    const hoursBtn = document.getElementById('hoursBtn')
+    const minutesBtn = document.getElementById('minutesBtn')
+    const secondsBtn = document.getElementById('secondsBtn')
 
-    console.log('TODO:...');
+    let daysInput = document.getElementById('days')
+    let hoursInput = document.getElementById('hours')
+    let minutesInput = document.getElementById('minutes')
+    let secondsInput = document.getElementById('seconds')
+
+    daysBtn.addEventListener('click', function() {
+        let days = daysInput.value
+        hoursInput.value = days * 24
+        minutesInput.value = days * 1440
+        secondsInput.value = days * 86400
+    })
+ 
+    hoursBtn.addEventListener('click', function() {
+        let hours = hoursInput.value
+        daysInput.value = hours / 24
+        minutesInput.value = hours * 60
+        secondsInput.value = hours * 60 * 60
+    })
+ 
+    minutesBtn.addEventListener('click', function() {
+        let minutes = minutesInput.value
+        hoursInput.value = minutes / 60
+        daysInput.value = minutes / 60 / 24
+        secondsInput.value = minutes * 60
+    })
+ 
+    secondsBtn.addEventListener('click', function() {
+        let seconds = secondsInput.value
+        hoursInput.value = seconds / 60 / 60
+        minutesInput.value = seconds / 60
+        daysInput.value = seconds / 60 / 60 / 24
+    })
 }
